baa100e81bdbd9c625f82366d035dd0044dd0c5c
Ahmed Benmoussa <ahmed.benmoussa@stu.ocsb.ca>
2024-12-18 23:22:41 +0000

    dsda

--- fish.py
+++ fish.py
@@ -2,3 +2,13 @@ from ursina import *
 import random
 
 class Fish:
+    def __init__(self, position=(0, 0, 0)):
+        self.entity = entity(
+            model='sphere',
+            color=color.blue
+            scale=0.3,
+            position=position
+        )
+
+        def respawn(self):
+            self.entity.position = (random.uniform(-3, 3), -2, 0)
