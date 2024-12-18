4de355938aad17902f2f17816501cfee7a11ecd8
Ahmed Benmoussa <ahmed.benmoussa@stu.ocsb.ca>
2024-12-18 23:27:05 +0000

    wuhJRGIQAURFKg

---
+++ rod.py
@@ -0,0 +1,13 @@
+from ursinaimport *
+class Rod:
+    def __init__(self, position=(0, 0, 0)):
+        self.entity = entity(
+            model='cube'
+            color=color.orange,
+            scale=(0.2, 1, 0.2),
+            position=position
+        )
+
+def move(self):
+    self.entity.x += held_keys['d'] * 0.05
+    self.entity.x += held_keys['a'] * 0.05
